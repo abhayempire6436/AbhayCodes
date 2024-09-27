88e5f5e2c5cecda8358f1c9e29fe95cbe0b69c53
Abhay <abhay.singh3104@gmail.com>
2024-09-27 11:44:15 +0530

    t

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Program</title>
+    
+</head>
+
+<body>
+    <div class="centre">
+
+    </div>
+    <?php
+        echo "Abhay Empire";
+        echo "<br>Welcome to the era of the Roman Empire"
+    ?>
+</body>
+</html>
+
